72e8adee7ccf14cfff947c1e317e0bf69f3a662c
Jaegyun Kim <jaegyunkim25@gmail.com>
2024-02-12 17:19:49 +0900

    export onnx

--- learning_spoons_lec/model/sentence_bert.py
+++ learning_spoons_lec/model/sentence_bert.py
@@ -1,4 +1,9 @@
+import torch
+from onnxruntime import SessionOptions, InferenceSession
 from sentence_transformers import SentenceTransformer
+import pdb
+
+from txtai.pipeline import HFOnnx
 
 """
 @misc{kr-sbert,
@@ -11,7 +16,7 @@ from sentence_transformers import SentenceTransformer
 }
 """
 
-# 다뤄짐
+
 class SentenceBert:
     def __init__(self, model_name="jhgan/ko-sroberta-multitask"):
         self.model = SentenceTransformer(model_name)
@@ -27,7 +32,33 @@ class SentenceBert:
         expected_embedding = SentenceTransformer(path).encode(sample_query)
         assert all([actual == expected for actual, expected in zip(actual_embedding, expected_embedding)])
 
+    def export_onnx(self, model_name, path):
+        # Preparing Input
+        sample_query = "러닝스푼즈"
+        model_inputs = self.model.tokenizer(sample_query, return_tensors="np")
+
+        onnx = HFOnnx()
+        # Converting Model
+        onnx_model = onnx(
+            model_name,
+            "pooling",
+            path,
+            quantize=False)
+        # ====================================
+        # # Implementation
+        # ====================================
+        options = SessionOptions()
+        session = InferenceSession(onnx_model, options)
+        onnx_output = session.run(None, dict(model_inputs))
+        actual_output = self.model.encode(sample_query)
+
+        onnx_tensor = torch.tensor(onnx_output[0][0]).view(1,-1)
+        actual_tensor = torch.tensor(actual_output).view(1,-1)
+        assert (1 == torch.cosine_similarity(onnx_tensor, actual_tensor))
+
+
 if __name__ == "__main__":
-    model = SentenceBert()
+    model = SentenceBert(model_name="intfloat/multilingual-e5-large")
     print(model.infer("learning spoons"))
-    model.export("/Users/jack/engine/resource/ST")
+    # model.export("/Users/jack/engine/learning_spoons_lec/resource/ST")
+    model.export_onnx("intfloat/multilingual-e5-large", "/Users/jack/engine/learning_spoons_lec/resource/st.onnx")
